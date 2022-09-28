98ba1149176ecb5b25dc220a8dae9fe3011d914d
Šimon Tupý <simontupy64@gmail.com>
2022-09-28 14:02:16 +0200

    Added a custom entry renderer.

---
+++ NasaRestAPI.Android/Renderers/CustomEntryRendererAndroid.cs
@@ -0,0 +1,46 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Graphics.Drawables;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using NasaRestAPI.Droid.Renderers;
+using NasaRestAPI.Renderers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly : ExportRenderer(typeof(CustomEntry), typeof(CustomEntryRendererAndroid))]
+namespace NasaRestAPI.Droid.Renderers
+{
+    public class CustomEntryRendererAndroid : EntryRenderer
+    {
+        public CustomEntryRendererAndroid(Context context) : base(context)
+        {
+
+        }
+
+        CustomEntry customEntry;
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if(e.OldElement == null)
+            {
+                customEntry = (CustomEntry)e.NewElement;
+                var gradientDrawable = new GradientDrawable();
+
+                gradientDrawable.SetCornerRadius(customEntry.CornerRadius);
+                gradientDrawable.SetStroke(customEntry.BorderWidth, customEntry.BorderColor.ToAndroid());
+
+                Control.SetBackground(gradientDrawable);
+                Control.SetPadding((int)customEntry.Padding.Left, (int)customEntry.Padding.Top, (int)customEntry.Padding.Right, (int)customEntry.Padding.Bottom);
+            }
+        }
+    }
+}
---
+++ NasaRestAPI/Renderers/CustomEntry.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace NasaRestAPI.Renderers
+{
+    public class CustomEntry : Entry
+    {
+        public static readonly BindableProperty CornerRadiusProperty =
+            BindableProperty.Create("CornerRadius", typeof(int), typeof(CustomEntry), 0);
+
+        public int CornerRadius
+        {
+            get { return (int)GetValue(CornerRadiusProperty); }
+            set { SetValue(CornerRadiusProperty, value); }
+        }
+
+        public static readonly BindableProperty BorderColorProperty =
+            BindableProperty.Create("BorderColor", typeof(Color), typeof(CustomEntry), Color.Red);
+
+        public Color BorderColor
+        {
+            get { return (Color)GetValue(BorderColorProperty); }
+            set { SetValue(BorderColorProperty, value); }
+        }
+
+        public static readonly BindableProperty BorderWidthProperty =
+           BindableProperty.Create("BorderWidth", typeof(int), typeof(CustomEntry), 2);
+
+        public int BorderWidth
+        {
+            get { return (int)GetValue(BorderWidthProperty); }
+            set { SetValue(BorderWidthProperty, value); }
+        }
+
+        public static readonly BindableProperty PaddingProperty =
+            BindableProperty.Create("Padding", typeof(Thickness), typeof(CustomEntry), new Thickness(0, 0, 0, 0));
+
+        public Thickness Padding
+        {
+            get { return (Thickness)GetValue(PaddingProperty); }
+            set { SetValue(PaddingProperty, value); }
+        }
+    }
+}
